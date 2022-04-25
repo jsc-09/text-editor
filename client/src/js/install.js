016bfd2142be3a4a98320efd04b913edb4b5ebf2
Joanna Christian <joanna.s.christian@gmail.com>
2022-04-25 09:46:59 -0700

    updated install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,7 +3,7 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
+    window.deferredPrompt = event;
     butInstall.style.visibility = 'visible';
     textHeader.textContent = 'Click the button to install!';
 });
@@ -11,7 +11,8 @@ window.addEventListener('beforeinstallprompt', (event) => {
 // TODO: Implement a click event handler on the `butInstall` element
 // butInstall.addEventListener('click', async () => {});
 butInstall.addEventListener('click', async () => {
-    event.prompt();
+    const promptEvent = window.deferredPrompt;
+    promptEvent.prompt();
     butInstall.setAttribute('disabled', true);
     butInstall.textContent = "Installed!";
 });
