6e08d3c6e271555cc2743a0f4403ccab83acc85a
Joanna Christian <joanna.s.christian@gmail.com>
2022-04-25 20:40:25 -0700

    revert back to original

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,28 +4,20 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false);
-    // butInstall.style.visibility = 'visible';
-    // textHeader.textContent = 'Click the button to install!';
+    butInstall.style.visibility = 'visible';
+    textHeader.textContent = 'Click the button to install!';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 // butInstall.addEventListener('click', async () => {});
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
-    if (!promptEvent) {
-        return;
-       }
     promptEvent.prompt();
-    window.deferredPrompt = null;
-    butInstall.classList.toggle('hidden', true);
-    // butInstall.setAttribute('disabled', true);
-    // butInstall.textContent = "Installed!";
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = "Installed!";
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log('👍', 'appinstalled', event);
-    window.deferredPrompt = null;
-
 });
